a641c38dbd784f82864a58767fcafa1a17a89141
Marek Kozaka <mk5498@nyu.edu>
2019-07-25 16:18:30 -0400

    added more functionality

--- backend.py
+++ backend.py
@@ -14,6 +14,28 @@ def insert(name, team, jersey, mvp):
     connection.commit()
     connection.close()
 
+def update(id, name, team, jersey, mvp):
+    connection = sqlite3.connect("Players.db")
+    cursor = connection.cursor()
+    cursor.execute("UPDATE Athlete SET name=?, team=?, jersey=?, mvp=? WHERE id=?", (name, team, jersey, mvp, id))
+    connection.commit()
+    connection.close()  
+
+def delete(id):
+    connection = sqlite3.connect("Players.db")
+    cursor = connection.cursor()
+    cursor.execute("DELETE FROM Athlete WHERE id=?", (id,))
+    connection.commit()
+    connection.close()  
+
+def search(name = "", team = "", jersey = "", mvp = ""):
+    connection = sqlite3.connect("Players.db")
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM Athlete WHERE name=? OR team=? OR jersey=? OR mvp=?", (name, team, jersey, mvp))
+    data = cursor.fetchall()
+    connection.close()
+    return data
+
 def view():
     connection = sqlite3.connect("Players.db")
     cursor = connection.cursor()
@@ -23,6 +45,3 @@ def view():
     return data
 
 connect()
-insert("Kobe Bryant", "Los Angeles Lakers", 24, 1)
-print(view())
-
--- frontend.py
+++ frontend.py
@@ -1,38 +1,53 @@
 from tkinter import *
 import backend
 
+def view_players():
+    results_box.delete(0, END)
+    for data in backend.view():
+        results_box.insert(END, data)
+
+def search_players():
+    results_box.delete(0, END)
+    for data in backend.search(name_text.get(), team_text.get(), jersey_text.get(), mvp_text.get()):
+        results_box.insert(END, data)
+    
+def add_player():
+    backend.insert(name_text.get(), team_text.get(), jersey_text.get(), mvp_text.get())
+    results_box.delete(0, END)
+    results_box.insert(END, (name_text.get(), team_text.get(), jersey_text.get(), mvp_text.get()))
+
 window = Tk()
 #window.configure(background = "grey")
 
 #Labels for each entry field
-title_label = Label(window, text="Name: ")
-title_label.grid(row = 0, column = 0)
+name_label = Label(window, text="Name: ")
+name_label.grid(row = 0, column = 0)
 
-author_label = Label(window, text="Team: ")
-author_label.grid(row = 0, column = 2)
+team_label = Label(window, text="Team: ")
+team_label.grid(row = 0, column = 2)
 
-pages_label = Label(window, text="Jersey Number: ")
-pages_label.grid(row = 1, column = 0)
+jersey_label = Label(window, text="Jersey Number: ")
+jersey_label.grid(row = 1, column = 0)
 
-year_label = Label(window, text="MVP Awards: ")
-year_label.grid(row = 1, column = 2)
+mvp_label = Label(window, text="MVP Awards: ")
+mvp_label.grid(row = 1, column = 2)
 
 #Entry fields for each label
-title_text = StringVar()
-title_entry = Entry(window, textvariable = name_text)
-title_entry.grid(row = 0, column = 1)
+name_text = StringVar()
+name_entry = Entry(window, textvariable = name_text)
+name_entry.grid(row = 0, column = 1)
 
-author_text = StringVar()
-author_entry = Entry(window, textvariable = team_text)
-author_entry.grid(row = 0, column = 3)
+team_text = StringVar()
+team_entry = Entry(window, textvariable = team_text)
+team_entry.grid(row = 0, column = 3)
 
-pages_text = StringVar()
-pages_entry = Entry(window, textvariable = jersey_text)
-pages_entry.grid(row = 1, column = 1)
+jersey_text = StringVar()
+jersey_entry = Entry(window, textvariable = jersey_text)
+jersey_entry.grid(row = 1, column = 1)
 
-year_text = StringVar()
-year_entry = Entry(window, textvariable = mvp_text)
-year_entry.grid(row = 1, column = 3)
+mvp_text = StringVar()
+mvp_entry = Entry(window, textvariable = mvp_text)
+mvp_entry.grid(row = 1, column = 3)
 
 #Text field where results are published (List Box)
 results_box = Listbox(window, height = 8, width = 38)
@@ -43,16 +58,16 @@ results_box.configure(yscrollcommand = scroll_bar.set)
 scroll_bar.configure(command = results_box.yview)
 
 #Buttons 
-view_button = Button(window, text = "View All", width = 12)
+view_button = Button(window, text = "View All", width = 12, command = view_players)
 view_button.grid(row = 3, column = 3)
 
-search_button = Button(window, text = "Search Player", width = 12)
+search_button = Button(window, text = "Search Player", width = 12, command = search_players)
 search_button.grid(row = 4, column = 3)
 
 update_button = Button(window, text = "Update Player", width = 12)
 update_button.grid(row = 5, column = 3)
 
-add_button = Button(window, text = "Add Player", width = 12)
+add_button = Button(window, text = "Add Player", width = 12, command = add_player)
 add_button.grid(row = 6, column = 3)
 
 delete_button = Button(window, text = "Delete Player", width = 12)
